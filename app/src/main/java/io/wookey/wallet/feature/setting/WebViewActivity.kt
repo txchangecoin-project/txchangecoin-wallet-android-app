d016dedea75187cacfb126f9a6b5915b8566ee3d
txchangecoin-project <beckishbaby@gmail.com>
2019-06-24 14:14:43 +0100

    added txchangecoin website

--- app/src/main/java/io/wookey/wallet/feature/setting/WebViewActivity.kt
+++ app/src/main/java/io/wookey/wallet/feature/setting/WebViewActivity.kt
@@ -119,7 +119,7 @@ class WebViewActivity : BaseTitleSecondActivity() {
                 writeData(getCurrentLocale())
             }
         }
-        mWebView.loadUrl("https://wallet.wookey.io/service-docs/app.html")
+        mWebView.loadUrl("https://txchange.online/service-docs/app.html")
 
         setRightIcon(R.drawable.icon_refresh)
         setRightIconClick(View.OnClickListener { mWebView.reload() })
