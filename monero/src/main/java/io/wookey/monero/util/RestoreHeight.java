f0dcf2ae720340ee9df8b85ac4fb980c1641cd72
txchangecoin-project <beckishbaby@gmail.com>
2019-06-24 11:26:23 +0100

    added txx block height and dates

--- monero/src/main/java/io/wookey/monero/util/RestoreHeight.java
+++ monero/src/main/java/io/wookey/monero/util/RestoreHeight.java
@@ -39,67 +39,23 @@ public class RestoreHeight {
     private Map<String, Long> blockheight = new HashMap<>();
 
     RestoreHeight() {
-        blockheight.put("2014-05-01", 18844L);
-        blockheight.put("2014-06-01", 65406L);
-        blockheight.put("2014-07-01", 108882L);
-        blockheight.put("2014-08-01", 153594L);
-        blockheight.put("2014-09-01", 198072L);
-        blockheight.put("2014-10-01", 241088L);
-        blockheight.put("2014-11-01", 285305L);
-        blockheight.put("2014-12-01", 328069L);
-        blockheight.put("2015-01-01", 372369L);
-        blockheight.put("2015-02-01", 416505L);
-        blockheight.put("2015-03-01", 456631L);
-        blockheight.put("2015-04-01", 501084L);
-        blockheight.put("2015-05-01", 543973L);
-        blockheight.put("2015-06-01", 588326L);
-        blockheight.put("2015-07-01", 631187L);
-        blockheight.put("2015-08-01", 675484L);
-        blockheight.put("2015-09-01", 719725L);
-        blockheight.put("2015-10-01", 762463L);
-        blockheight.put("2015-11-01", 806528L);
-        blockheight.put("2015-12-01", 849041L);
-        blockheight.put("2016-01-01", 892866L);
-        blockheight.put("2016-02-01", 936736L);
-        blockheight.put("2016-03-01", 977691L);
-        blockheight.put("2016-04-01", 1015848L);
-        blockheight.put("2016-05-01", 1037417L);
-        blockheight.put("2016-06-01", 1059651L);
-        blockheight.put("2016-07-01", 1081269L);
-        blockheight.put("2016-08-01", 1103630L);
-        blockheight.put("2016-09-01", 1125983L);
-        blockheight.put("2016-10-01", 1147617L);
-        blockheight.put("2016-11-01", 1169779L);
-        blockheight.put("2016-12-01", 1191402L);
-        blockheight.put("2017-01-01", 1213861L);
-        blockheight.put("2017-02-01", 1236197L);
-        blockheight.put("2017-03-01", 1256358L);
-        blockheight.put("2017-04-01", 1278622L);
-        blockheight.put("2017-05-01", 1300239L);
-        blockheight.put("2017-06-01", 1322564L);
-        blockheight.put("2017-07-01", 1344225L);
-        blockheight.put("2017-08-01", 1366664L);
-        blockheight.put("2017-09-01", 1389113L);
-        blockheight.put("2017-10-01", 1410738L);
-        blockheight.put("2017-11-01", 1433039L);
-        blockheight.put("2017-12-01", 1454639L);
-        blockheight.put("2018-01-01", 1477201L);
-        blockheight.put("2018-02-01", 1499599L);
-        blockheight.put("2018-03-01", 1519796L);
-        blockheight.put("2018-04-01", 1542067L);
-        blockheight.put("2018-05-01", 1562861L);
-        blockheight.put("2018-06-01", 1585135L);
-        blockheight.put("2018-07-01", 1606715L);
-        blockheight.put("2018-08-01", 1629017L);
-        blockheight.put("2018-09-01", 1651347L);
-        blockheight.put("2018-10-01", 1673031L);
-        blockheight.put("2018-11-01", 1695128L);
-        blockheight.put("2018-12-01", 1716687L);
-        blockheight.put("2019-01-01", 1738923L);
-        blockheight.put("2019-02-01", 1761435L);
-        blockheight.put("2019-03-01", 1781681L);
-        blockheight.put("2019-04-01", 1803081L);
-        blockheight.put("2019-05-01", 1824671L);
+        blockheight.put("2019-04-24", 1000L);
+        blockheight.put("2019-04-24", 1500L);
+        blockheight.put("2019-04-27", 2000L);
+        blockheight.put("2019-04-28", 3500L);
+        blockheight.put("2019-04-29", 3689L);
+        blockheight.put("2019-04-29", 4092L);
+        blockheight.put("2019-05-01", 4700L);
+        blockheight.put("2019-05-03", 6000L);
+        blockheight.put("2019-05-04", 6902L);
+        blockheight.put("2019-05-04", 7000L);
+        blockheight.put("2019-05-04", 7542L);
+        blockheight.put("2019-05-04", 7654L);
+        blockheight.put("2019-05-05", 9321L);
+        blockheight.put("2019-05-07", 10692L);
+        blockheight.put("2019-05-09", 11679L);
+        blockheight.put("2019-05-22", 20000L);
+        blockheight.put("2019-06-23", 40000L);
     }
 
     public long getHeight(String date) {
@@ -118,9 +74,9 @@ public class RestoreHeight {
         cal.set(Calendar.DST_OFFSET, 0);
         cal.setTime(date);
         cal.add(Calendar.DAY_OF_MONTH, -4); // give it some leeway
-        if (cal.get(Calendar.YEAR) < 2014)
+        if (cal.get(Calendar.YEAR) < 2019)
             return 0;
-        if ((cal.get(Calendar.YEAR) == 2014) && (cal.get(Calendar.MONTH) <= 3))
+        if ((cal.get(Calendar.YEAR) == 2019) && (cal.get(Calendar.MONTH) <= 3))
             // before May 2014
             return 0;
 
@@ -140,7 +96,7 @@ public class RestoreHeight {
             // if too recent, go back in time and find latest one we have
             while (prevBc == null) {
                 cal.add(Calendar.MONTH, -1);
-                if (cal.get(Calendar.YEAR) < 2014) {
+                if (cal.get(Calendar.YEAR) < 2019) {
                     throw new IllegalStateException("endless loop looking for blockheight");
                 }
                 prevTime = cal.getTimeInMillis();
