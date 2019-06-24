a1ab37df1a299ed25015f36ef6f2d8f0ded65675
txchangecoin-project <beckishbaby@gmail.com>
2019-06-24 11:09:44 +0100

    added txchangecoin name

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ cd /opt/android
 wget https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip -O android-ndk-r17c-linux-x86_64.zip && echo "3f541adbd0330a9205ba12697f6d04ec90752c53d6b622101a2a8a856e816589  android-ndk-r17c-linux-x86_64.zip"|sha256sum -c
 unzip -q android-ndk-r17c-linux-x86_64.zip
 mv android-ndk-r17c ndk-r17c
-git clone https://github.com/WooKeyWallet/monero-android-lib.git /tmp/monero-android-lib
-cd /tmp/monero-android-lib/external-libs/
+git clone https://github.com/txchangecoin-project/txchangecoin-android-lib.git /tmp/txchangecoin-android-lib
+cd /tmp/txchangecoin-android-lib/external-libs/
 find . -name "*.sh" -exec chmod +x {} \;
 mkdir -p build/src
 make all
